32d7336470d717f479d1715073e026dd64a74493
ulises <ulisessaix@gmail.com>
2024-10-03 15:33:55 -0600

    se modifica codigo de otros 6

--- Codigo6.java
+++ Codigo6.java
@@ -1,24 +1,37 @@
+import java.util.Scanner;
+
 public class codigo6 {
-	  
-	    int[] n = int[20];
+	  public static void main(String[] args) {
+		
+		  int[] n = new int[20];//se agrega palabra new
+		  
+		  for (int i = 0; i < 20; i++) {//se añade simbolo +
+			  n[i] = (int)(Math.random() * 381) + 20;
+			  System.out.print(n[i] + " ");//se añade out
+		  }//for
+		  
+		  System.out.println("\n¿Qué números quiere resaltar? ");
+		  System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+		  
+		  Scanner s = new Scanner(System.in);
+		  int opcion = parseInt(s.nextInt());
+		  
+		  
+		  int multiplo = (opcion == 1)?5:7;
+		  
+		  for (int e : n) {
+			  if (e % multiplo == 0) {
+				  System.out.print("[" + e + "] ");
+			  }else {
+					  System.out.println(e + " ");
+				  }//else
+			  }//if
+		  s.close();
+	}//main
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
-	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
-
-	    int multiplo = (opcion == 1) : 5 ? 7;
-
-	    foreach (char e : n) {
-	      if (e % multiplo == 0) {
-	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
-	      }
-	    }
-	  
+	private static int parseInt(int nextInt) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
+	  
+	}//class codigo6
